eb67db46caed0a21669e4209a66d7aad25787b5a
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-06-08 22:13:34 -0300

    fix: controller inject dependencies usuario

--- app/Controller/Api/V1/Usuario/UsuarioController.php
+++ app/Controller/Api/V1/Usuario/UsuarioController.php
@@ -2,6 +2,16 @@
 
 namespace App\Controller\Api\V1\Usuario;
 
+use App\Api\V1\DB\Transaction\Transaction;
+use App\Api\V1\Usuario\Admin\AdminPersistence;
+use App\Api\V1\Usuario\Admin\Exception\AdminException;
+use App\Api\V1\Usuario\Admin\Validation\AdminValidation;
+use App\Api\V1\Usuario\Cliente\ClientePersistence;
+use App\Api\V1\Usuario\Cliente\Validation\ClienteValidation;
+use App\Api\V1\Usuario\Exception\UsuarioException;
+use App\Api\V1\Usuario\UsuarioPersistence;
+use App\Api\V1\Usuario\Validation\UsuarioValidation;
+use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
 
@@ -10,6 +20,27 @@ use Hyperf\HttpServer\Contract\ResponseInterface;
  */
 class UsuarioController
 {
+    #[Inject]
+    protected UsuarioValidation $usuarioValidation;
+
+    #[Inject]
+    protected AdminValidation $adminValidation;
+
+    #[Inject]
+    protected ClienteValidation $clienteValidation;
+
+    #[Inject]
+    protected UsuarioPersistence $usuarioPersistence;
+
+    #[Inject]
+    protected AdminPersistence $adminPersistence;
+
+    #[Inject]
+    protected ClientePersistence $clientePersistence;
+
+    #[Inject]
+    protected Transaction $transaction;
+
     /**
      * Method constructor
      *
@@ -29,14 +60,47 @@ class UsuarioController
     public function register(RequestInterface $request, ResponseInterface $response)
     {
         try {
-            $params = [
+            $inputs = [
                 'nome' => $request->input('nome'),
                 'email' => $request->input('email'),
                 'senha' => $request->input('senha'),
                 'isAdmin' => $request->input('isAdmin')
             ];
-            return $params;
+
+            $this->usuarioValidation->validate($inputs);
+
+            $this->transaction->beginTransaction();
+            $userId = $this->usuarioPersistence->insertGetId($inputs);
+
+            if ($inputs['isAdmin']) {
+                $this->adminValidation->validate(['userId' => $userId]);
+                $this->adminPersistence->insert($userId);
+            }
+            $this->clienteValidation->validate(['userId' => $userId]);
+            $this->clientePersistence->insert($userId);
+            $this->transaction->commit();
+
+            return $response->json([
+                'success' => true,
+                'message' => 'Usuário inserido com sucesso.'
+            ])->withStatus(201);
+        } catch (\InvalidArgumentException $in) {
+            return $response->json(json_decode($in->getMessage()))->withStatus(422);
+        } catch (UsuarioException $ue) {
+            $this->transaction->rollBack();
+            return $response->json(([
+                'errors' => $ue->getMessage()
+            ]))->withStatus(400);
+        } catch (AdminException $ae) {
+            $this->transaction->rollBack();
+            return $response->json(([
+                'errors' => $ae->getMessage()
+            ]))->withStatus(400);
         } catch (\Throwable $th) {
+            $this->transaction->rollBack();
+            return $response->json(([
+                'errors' => $th->getMessage()
+            ]))->withStatus(500);
         }
     }
 }
