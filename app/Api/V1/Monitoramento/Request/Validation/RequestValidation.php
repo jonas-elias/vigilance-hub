248b092125b18eb9f605331657ff3092a6bb1e82
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-06-25 01:26:30 -0300

    fix: update method validation request

--- app/Api/V1/Monitoramento/Request/Validation/RequestValidation.php
+++ app/Api/V1/Monitoramento/Request/Validation/RequestValidation.php
@@ -13,19 +13,21 @@ class RequestValidation extends Validator
      * Method to validate application inputs
      *
      * @param array $inputs
+     * @param ?string $method = null
      * @return void
      */
-    public function validate(array $inputs): void
+    public function validate(array $inputs, ?string $method = null): void
     {
         $validation = $this->validator->make([
-            'idMonitoramento' => $inputs['idMonitoramento'],
-            'duracao' => $inputs['duracao'],
-            'status' => $inputs['status'],
-            'metodo' => $inputs['metodo'],
-            'uri' => $inputs['uri'],
-            'headers' => $inputs['headers'],
-            'response' => $inputs['response']
-        ], $this->rules());
+            'idMonitoramento' => $inputs['idMonitoramento'] ?? '',
+            'duracao' => $inputs['duracao'] ?? '',
+            'status' => $inputs['status'] ?? '',
+            'metodo' => $inputs['metodo'] ?? '',
+            'uri' => $inputs['uri'] ?? '',
+            'headers' => $inputs['headers'] ?? '',
+            'response' => $inputs['response'] ?? '',
+            'idRequest' => $inputs['idRequest'] ?? ''
+        ], $this->rules()[$method]);
 
         if ($validation->fails()) {
             throw new \InvalidArgumentException(json_encode($validation->errors()));
@@ -40,13 +42,24 @@ class RequestValidation extends Validator
     public function rules(): array
     {
         return [
-            'idMonitoramento' => 'required|integer|exists:monitoramento,id',
-            'duracao' => 'required|numeric',
-            'status' => 'required|integer',
-            'metodo' => 'required|string|max:7',
-            'uri' => 'required|string|max:255',
-            'headers' => 'required|string|max:8192',
-            'response' => 'required|string|max:8192'
+            'insert' => [
+                'idMonitoramento' => 'required|integer|exists:monitoramento,id',
+                'duracao' => 'required|numeric',
+                'status' => 'required|integer',
+                'metodo' => 'required|string|max:7',
+                'uri' => 'required|string|max:255',
+                'headers' => 'required|string|max:8192',
+                'response' => 'required|string|max:8192'
+            ],
+            'update' => [
+                'idRequest' => 'required|integer|exists:request,id',
+                'duracao' => 'required|numeric',
+                'status' => 'required|integer',
+                'metodo' => 'required|string|max:7',
+                'uri' => 'required|string|max:255',
+                'headers' => 'required|string|max:8192',
+                'response' => 'required|string|max:8192'
+            ]
         ];
     }
 }
