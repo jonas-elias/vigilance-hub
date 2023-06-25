7e4326f27e9e427dd0c368b3949b09d4263015fa
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-06-25 01:29:44 -0300

    feat: update method in depuracao persistence

--- app/Api/V1/Monitoramento/Depuracao/DepuracaoPersistence.php
+++ app/Api/V1/Monitoramento/Depuracao/DepuracaoPersistence.php
@@ -31,4 +31,28 @@ class DepuracaoPersistence extends Database
             ]));
         }
     }
+
+    /**
+     * Method to update depuracao monitoramento in database
+     *
+     * @param array $inputs
+     * @param int $idDepuracao
+     * @return void
+     */
+    public function update(array $inputs, int $idDepuracao): void
+    {
+        try {
+            $this->getDb()->table('depuracao')
+                ->where('id', '=', $idDepuracao)
+                ->update([
+                    'depuracao' => $inputs['depuracao'],
+                ]);
+        } catch (\Throwable $th) {
+            throw new DepuracaoException(json_encode([
+                'erros' => [
+                    'Erro ao alterar o monitoramento de depuração.'
+                ]
+            ]));
+        }
+    }
 }
