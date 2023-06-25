bf95a199773b685c2b4573371df85480e07f1f6c
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-06-25 01:26:18 -0300

    fix: update method validation monitoramento

--- app/Api/V1/Monitoramento/Validation/MonitoramentoValidation.php
+++ app/Api/V1/Monitoramento/Validation/MonitoramentoValidation.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Api\V1\Monitoramento\Validation;
 
 use App\Api\V1\Validator\Validator;
@@ -13,14 +15,16 @@ class MonitoramentoValidation extends Validator
      * Method to validate application inputs
      *
      * @param array $inputs
+     * @param ?string $method = null
      * @return void
      */
-    public function validate(array $inputs): void
+    public function validate(array $inputs, ?string $method = null): void
     {
         $validation = $this->validator->make([
-            'clienteToken' => $inputs['clienteToken'],
-            'aplicacaoToken' => $inputs['aplicacaoToken']
-        ], $this->rules());
+            'clienteToken' => $inputs['clienteToken'] ?? '',
+            'aplicacaoToken' => $inputs['aplicacaoToken'] ?? '',
+            'idMonitoramento' => $inputs['idMonitoramento'] ?? ''
+        ], $this->rules()[$method]);
 
         if ($validation->fails()) {
             throw new \InvalidArgumentException(json_encode($validation->errors()));
@@ -35,8 +39,17 @@ class MonitoramentoValidation extends Validator
     public function rules(): array
     {
         return [
-            'clienteToken' => 'required|string|exists:cliente,token',
-            'aplicacaoToken' => 'required|string|exists:aplicacao,token'
+            'insert' => [
+                'clienteToken' => 'required|string|exists:cliente,token',
+                'aplicacaoToken' => 'required|string|exists:aplicacao,token'
+            ],
+            'delete' => [
+                'idMonitoramento' => 'required|integer|exists:monitoramento,id'
+            ],
+            'update' => [
+                'clienteToken' => 'required|string|exists:cliente,token',
+                'aplicacaoToken' => 'required|string|exists:aplicacao,token'
+            ] 
         ];
     }
 }
