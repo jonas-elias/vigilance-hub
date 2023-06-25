f7df6febd4e29889ff0de9b8a227ee2c84c21b23
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-06-25 01:24:45 -0300

    feat: update log monitoramento method

--- app/Controller/Api/V1/Monitoramento/Log/LogController.php
+++ app/Controller/Api/V1/Monitoramento/Log/LogController.php
@@ -54,17 +54,64 @@ class LogController
         ];
 
         try {
-            $this->monitoramentoValidation->validate($inputsMonitoramento);
+            $this->monitoramentoValidation->validate($inputsMonitoramento, 'insert');
             $this->transaction->beginTransaction();
             $id = $this->monitoramentoPersistence->insertGetId($inputsMonitoramento);
             $inputsLog = array_merge($inputsLog, ['idMonitoramento' => $id]);
-            $this->logValidation->validate($inputsLog);
+            $this->logValidation->validate($inputsLog, 'insert');
             $this->logPersistence->insertGetId($inputsLog);
             $this->transaction->commit();
             return $response->json([
                 'success' => true,
                 'message' => 'Monitoramento de log criado com sucesso.'
             ])->withStatus(201);
+        } catch (\InvalidArgumentException $ie) {
+            $this->transaction->rollBack();
+            return $response->json(json_decode($ie->getMessage()))->withStatus(400);
+        } catch (MonitoramentoException $ae) {
+            $this->transaction->rollBack();
+            return $response->json(json_decode($ae->getMessage()))->withStatus(400);
+        } catch (LogException $de) {
+            $this->transaction->rollBack();
+            return $response->json(json_decode($de->getMessage()))->withStatus(400);
+        } catch (\Throwable $th) {
+            $this->transaction->rollBack();
+            return $response->json(([
+                'errors' => 'Ocorreu algum erro interno na aplicação.'
+            ]))->withStatus(500);
+        }
+    }
+
+    /**
+     * Method to create log monitoramento
+     *
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     * @param int $idLog
+     */
+    public function update(RequestInterface $request, ResponseInterface $response, int $idLog)
+    {
+        $inputsMonitoramento = [
+            'aplicacaoToken' => $request->header('aplicacaoToken'),
+            'clienteToken' => $request->header('clienteToken')
+        ];
+
+        $inputsLog = [
+            'nivel' => $request->input('nivel'),
+            'mensagem' => $request->input('mensagem'),
+            'idLog' => $idLog
+        ];
+
+        try {
+            $this->monitoramentoValidation->validate($inputsMonitoramento, 'update');
+            $this->logValidation->validate($inputsLog, 'update');
+            $this->transaction->beginTransaction();
+            $this->logPersistence->update($inputsLog, $inputsLog['idLog']);
+            $this->transaction->commit();
+            return $response->json([
+                'success' => true,
+                'message' => 'Monitoramento de log alterado com sucesso.'
+            ])->withStatus(200);
         } catch (\InvalidArgumentException $ie) {
             $this->transaction->rollBack();
             return $response->json(json_decode($ie->getMessage()))->withStatus(400);
