9b24395fd44b1fc8cbe162bd9bfa52733d1c9daa
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-06-25 01:23:30 -0300

    feat: update request monitoramento

--- app/Controller/Api/V1/Monitoramento/Request/RequestController.php
+++ app/Controller/Api/V1/Monitoramento/Request/RequestController.php
@@ -55,17 +55,68 @@ class RequestController
         ];
 
         try {
-            $this->monitoramentoValidation->validate($inputsMonitoramento);
+            $this->monitoramentoValidation->validate($inputsMonitoramento, 'insert');
             $this->transaction->beginTransaction();
             $id = $this->monitoramentoPersistence->insertGetId($inputsMonitoramento);
             $inputsRequest = array_merge($inputsRequest, ['idMonitoramento' => $id]);
-            $this->requestValidation->validate($inputsRequest);
+            $this->requestValidation->validate($inputsRequest, 'insert');
             $this->requestPersistence->insertGetId($inputsRequest);
             $this->transaction->commit();
             return $response->json([
                 'success' => true,
                 'message' => 'Monitoramento da request criado com sucesso.'
             ])->withStatus(201);
+        } catch (\InvalidArgumentException $ie) {
+            $this->transaction->rollBack();
+            return $response->json(json_decode($ie->getMessage()))->withStatus(400);
+        } catch (MonitoramentoException $ae) {
+            $this->transaction->rollBack();
+            return $response->json(json_decode($ae->getMessage()))->withStatus(400);
+        } catch (RequestException $de) {
+            $this->transaction->rollBack();
+            return $response->json(json_decode($de->getMessage()))->withStatus(400);
+        } catch (\Throwable $th) {
+            $this->transaction->rollBack();
+            return $response->json(([
+                'errors' => 'Ocorreu algum erro interno na aplicação.'
+            ]))->withStatus(500);
+        }
+    }
+
+    /**
+     * Method to update request monitoramento
+     *
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     * @param int $idRequest
+     */
+    public function update(RequestInterface $request, ResponseInterface $response, int $idRequest)
+    {
+        $inputsMonitoramento = [
+            'aplicacaoToken' => $request->header('aplicacaoToken'),
+            'clienteToken' => $request->header('clienteToken')
+        ];
+
+        $inputsRequest = [
+            'duracao' => $request->input('duracao'),
+            'status' => $request->input('status'),
+            'metodo' => $request->input('metodo'),
+            'uri' => $request->input('uri'),
+            'headers' => $request->input('headers'),
+            'response' => $request->input('response'),
+            'idRequest' => $idRequest
+        ];
+
+        try {
+            $this->monitoramentoValidation->validate($inputsMonitoramento, 'update');
+            $this->requestValidation->validate($inputsRequest, 'update');
+            $this->transaction->beginTransaction();
+            $this->requestPersistence->update($inputsRequest, $inputsRequest['idRequest']);
+            $this->transaction->commit();
+            return $response->json([
+                'success' => true,
+                'message' => 'Monitoramento da request alterado com sucesso.'
+            ])->withStatus(200);
         } catch (\InvalidArgumentException $ie) {
             $this->transaction->rollBack();
             return $response->json(json_decode($ie->getMessage()))->withStatus(400);
