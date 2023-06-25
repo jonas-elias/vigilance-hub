dee19a4c98f73a9c03b8f7c7baed5c76c7cd92d9
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-06-25 01:25:56 -0300

    fix: update validation usuario

--- app/Api/V1/Usuario/Validation/UsuarioValidation.php
+++ app/Api/V1/Usuario/Validation/UsuarioValidation.php
@@ -13,16 +13,17 @@ class UsuarioValidation extends Validator
      * Method to validation inputs usuario
      *
      * @param array $inputs
+     * @param string $method
      * @return void
      */
-    public function validate(array $inputs): void
+    public function validate(array $inputs, ?string $method = null): void
     {
         $validation = $this->validator->make([
-            'nome' => $inputs['nome'],
-            'email' => $inputs['email'],
-            'senha' => $inputs['senha'],
-            'isAdmin' => $inputs['isAdmin']
-        ], $this->rules());
+            'nome' => $inputs['nome'] ?? '',
+            'email' => $inputs['email'] ?? '',
+            'senha' => $inputs['senha'] ?? '',
+            'isAdmin' => $inputs['isAdmin'] ?? ''
+        ], $this->rules()[$method]);
 
         if ($validation->fails()) {
             throw new \InvalidArgumentException(json_encode($validation->errors()));
@@ -37,10 +38,16 @@ class UsuarioValidation extends Validator
     public function rules(): array
     {
         return [
-            'nome' => 'required|string|max:255',
-            'email' => 'required|string|email|unique:usuario,email|max:255',
-            'senha' => 'required|string|max:255',
-            'isAdmin' => 'required|boolean'
+            'update' => [
+                'nome' => 'required|string|max:255',
+                'email' => 'required|string|email|unique:usuario,email|max:255',
+            ],
+            'insert' => [
+                'nome' => 'required|string|max:255',
+                'email' => 'required|string|email|unique:usuario,email|max:255',
+                'senha' => 'required|string|max:255',
+                'isAdmin' => 'required|boolean'
+            ]
         ];
     }
 }
