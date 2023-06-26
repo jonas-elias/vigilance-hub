c4ca1b30cfa8fd4198b30bef8147fcba590124a6
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-06-26 01:54:35 -0300

    feat: client python

---
+++ client/app.py
@@ -0,0 +1,354 @@
+from rich.console import Console
+from rich.panel import Panel
+import client_vigilance_hub
+from time import sleep
+import plotly.graph_objects as go
+import pandas as pd
+import json
+
+
+def call_api(params, function):
+    function_to_call = getattr(client_vigilance_hub, function)
+    console.print(f"[{option_style}]Resposta:")
+    response = function_to_call(params)
+    console.print(response)
+    return response
+
+
+while True:
+    console = Console()
+
+    title_style = "bold underline"
+    option_style = "bold cyan"
+    success_style = "bold green"
+
+    content = Panel(
+        f"[{title_style}]Opções:[/{title_style}]\n\n"
+        f"[{option_style}]1. Opções de insert\n"
+        f"[{option_style}]2. Opções de update\n"
+        f"[{option_style}]3. Opções de delete\n"
+        f"[{option_style}]4. Opções de geração gráfico\n"
+    )
+
+    console.print(content)
+
+    option = console.input("\nEscolha uma opção: ")
+
+    if option == "1":
+        content = Panel(
+            f"\n"
+            f"[{title_style}]Opções de insert:[/{title_style}]\n\n"
+            f"[{option_style}]1. Criar usuário\n"
+            f"[{option_style}]2. Criar aplicação\n"
+            f"[{option_style}]3. Criar monitoramento de cache\n"
+            f"[{option_style}]4. Criar monitoramento de request\n"
+            f"[{option_style}]5. Criar monitoramento de depuração\n"
+            f"[{option_style}]6. Criar monitoramento de erro\n"
+            f"[{option_style}]7. Criar monitoramento de log\n"
+            f"[{option_style}]8. Criar monitoramento de query"
+        )
+
+        console.print(content)
+
+        sub_option = console.input("\nEscolha uma opção: ")
+
+        if sub_option == "1":
+            user_data = {
+                "nome": console.input("Nome: "),
+                "email": console.input("Email: "),
+                "senha": console.input("Senha: "),
+                "isAdmin": console.input("isAdmin (True/False): ").lower() == "true"
+            }
+            call_api(user_data, 'create_user')
+            continue
+        elif sub_option == "2":
+            application_data = {
+                "idCliente": console.input("ID do Cliente: "),
+                "nome": console.input("Nome: ")
+            }
+            call_api(application_data, 'create_application')
+            continue
+        elif sub_option == "3":
+            cache_data = {
+                "acao": console.input("Ação: "),
+                "chave": console.input("Chave: "),
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            call_api(cache_data, 'create_cache_monitoring')
+            continue
+        elif sub_option == "4":
+            request_data = {
+                "duracao": float(console.input("Duração: ")),
+                "status": int(console.input("Status: ")),
+                "metodo": console.input("Método: "),
+                "uri": console.input("URI: "),
+                "headers": console.input("Headers: "),
+                "response": console.input("Response: "),
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            call_api(request_data, 'create_request_monitoring')
+            continue
+        elif sub_option == "5":
+            debug_data = {
+                "depuracao": console.input("Depuração: "),
+                "headers": console.input("Headers: "),
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            call_api(debug_data, 'create_debug_monitoring')
+            continue
+        elif sub_option == "6":
+            error_data = {
+                "nivel": console.input("Nível: "),
+                "localizacao": console.input("Localização: "),
+                "stacktrace": console.input("Stacktrace: "),
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            call_api(error_data, 'create_error_monitoring')
+            continue
+        elif sub_option == "7":
+            log_data = {
+                "nivel": console.input("Nível: "),
+                "mensagem": console.input("Mensagem: "),
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            call_api(log_data, 'create_log_monitoring')
+            continue
+        elif sub_option == "8":
+            query_data = {
+                "duracao": float(console.input("Duração: ")),
+                "conector": console.input("Conector: "),
+                "localizacao": console.input("Localização: "),
+                "query": console.input("Query: "),
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            call_api(query_data, 'create_query_monitoring')
+            continue
+        else:
+            console.print(f"[{'bold red'}]Opção inválida.\n")
+            sleep(1)
+            continue
+
+    if option == "2":
+        content = Panel(
+            f"\n"
+            f"[{title_style}]Opções de update:[/{title_style}]\n\n"
+            f"[{option_style}]1. Alterar usuário\n"
+            f"[{option_style}]2. Alterar aplicação\n"
+            f"[{option_style}]3. Alterar monitoramento de cache\n"
+            f"[{option_style}]4. Alterar monitoramento de request\n"
+            f"[{option_style}]5. Alterar monitoramento de depuração\n"
+            f"[{option_style}]6. Alterar monitoramento de erro\n"
+            f"[{option_style}]7. Alterar monitoramento de log\n"
+            f"[{option_style}]8. Alterar monitoramento de query"
+        )
+
+        console.print(content)
+
+        sub_option = console.input("\nEscolha uma opção: ")
+
+        if sub_option == "1":
+            user_data = {
+                "id_usuario": console.input("Id do usuário: "),
+                "nome": console.input("Nome: "),
+                "email": console.input("Email: ")
+            }
+            call_api(user_data, 'update_user')
+            continue
+        elif sub_option == "2":
+            application_data = {
+                "id_aplicacao": console.input("Id da aplicação: "),
+                "nome": console.input("Nome: ")
+            }
+            call_api(application_data, 'update_application')
+            continue
+        elif sub_option == "3":
+            cache_data = {
+                "id_cache": console.input("Id do monitoramento de cache: "),
+                "acao": console.input("Ação: "),
+                "chave": console.input("Chave: "),
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            call_api(cache_data, 'update_monitoramento_cache')
+            continue
+        elif sub_option == "4":
+            request_data = {
+                'id_request': console.input("Id do monitoramento da request: "),
+                "duracao": float(console.input("Duração: ")),
+                "status": int(console.input("Status: ")),
+                "metodo": console.input("Método: "),
+                "uri": console.input("URI: "),
+                "headers": console.input("Headers: "),
+                "response": console.input("Response: "),
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            call_api(request_data, 'update_monitoramento_request')
+            continue
+        elif sub_option == "5":
+            debug_data = {
+                'id_depuracao': console.input("Id do monitoramento da depuração: "),
+                "depuracao": console.input("Depuração: "),
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            call_api(debug_data, 'update_monitoramento_depuracao')
+            continue
+        elif sub_option == "6":
+            error_data = {
+                'id_erro': console.input("Id do monitoramento de erro: "),
+                "nivel": console.input("Nível: "),
+                "localizacao": console.input("Localização: "),
+                "stacktrace": console.input("Stacktrace: "),
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            call_api(error_data, 'update_monitoramento_erro')
+            continue
+        elif sub_option == "7":
+            log_data = {
+                'id_log': console.input("Id do monitoramento de log: "),
+                "nivel": console.input("Nível: "),
+                "mensagem": console.input("Mensagem: "),
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            call_api(log_data, 'update_monitoramento_log')
+            continue
+        elif sub_option == "8":
+            query_data = {
+                "id_query": console.input("Id do monitoramento de query: "),
+                "duracao": float(console.input("Duração: ")),
+                "conector": console.input("Conector: "),
+                "localizacao": console.input("Localização: "),
+                "query": console.input("Query: "),
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            call_api(query_data, 'update_monitoramento_query')
+            continue
+        else:
+            console.print("Opção inválida.\n")
+            sleep(1)
+            continue
+
+
+    if option == "3":
+        content = Panel(
+            f"\n"
+            f"[{title_style}]Opções de delete:[/{title_style}]\n\n"
+            f"[{option_style}]1. Deletar admin\n"
+            f"[{option_style}]2. Deletar cliente\n"
+            f"[{option_style}]3. Deletar aplicação\n"
+            f"[{option_style}]4. Deletar monitoramento\n"
+        )
+
+        console.print(content)
+
+        sub_option = console.input("\nEscolha uma opção: ")
+
+        if sub_option == "1":
+            admin_data = {
+                "id_admin": console.input("Id do usuário admin: ")
+            }
+            call_api(admin_data, 'delete_user_admin')
+            continue
+        elif sub_option == "2":
+            client_data = {
+                "id_cliente": console.input("Id do usuário cliente: ")
+            }
+            call_api(client_data, 'delete_user_cliente')
+            continue
+        elif sub_option == "3":
+            application_data = {
+                "id_aplicacao": console.input("Id da aplicação: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            call_api(application_data, 'delete_application')
+            continue
+        elif sub_option == "4":
+            monitoramento_data = {
+                "id_monitoramento": console.input("Id do monitoramento: "),
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            call_api(monitoramento_data, 'delete_monitoramento')
+            continue
+        else:
+            console.print(f"[{'bold red'}]Opção inválida.\n")
+            sleep(1)
+            continue
+    if option == "4":
+        content = Panel(
+            f"\n"
+            f"[{title_style}]Opções de geração de gráfico:[/{title_style}]\n\n"
+            f"[{option_style}]1. Gerar total de aplicações por cliente\n"
+            f"[{option_style}]2. Gerar total de monitoramentos\n"
+            f"[{option_style}]3. Gerar monitoramento por data\n"
+        )
+
+        console.print(content)
+
+        sub_option = console.input("\nEscolha uma opção: ")
+
+        if sub_option == "1":
+        
+            data = call_api([], 'total_aplicacoes')
+            df = pd.DataFrame(data)
+            id_cliente = df['id_cliente']
+            nome = df['nome']
+            aplicacoes = df['aplicacoes']
+
+            fig = go.Figure(data=[go.Bar(x=nome, y=aplicacoes, textposition='auto')])
+
+            fig.update_layout(
+                title='Número de Aplicações por Cliente',
+                xaxis=dict(title='ID Cliente'),
+                yaxis=dict(title='Número de Aplicações')
+            )
+            fig.show()
+            continue
+        elif sub_option == "2":
+            total_monitoramentos_data = {
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            data = call_api(total_monitoramentos_data, 'total_monitoramentos')
+
+            labels = list(data[0].keys())[3:]
+            values = list(data[0].values())[3:]
+
+            fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+
+            fig.update_layout(
+                title='Total de monitoramentos - Descreve quantos monitoramentos foram adicionados com base no token do cliente e no token da aplicação.')
+            fig.show()
+            continue
+        elif sub_option == "3":
+            total_monitoramento_data = {
+                "aplicacaoToken": console.input("Aplicação Token: "),
+                "clienteToken": console.input("Cliente Token: ")
+            }
+            data = call_api(total_monitoramento_data, 'total_monitoramento_data')
+            df = pd.DataFrame(data)
+            df['monitoramento_total'] = df['monitoramento_total'].astype(int)
+            fig = go.Figure(data=go.Scatter(x=df['data_monitoramento'], y=df['monitoramento_total']))
+            fig.update_layout(
+                title='Total de monitoramentos criados a partir de datas crescentes',
+                xaxis=dict(title='Data de inserção do monitoramento'),
+                yaxis=dict(title='Monitoramentos')
+            )
+            fig.show()
+            continue
+        else:
+            console.print(f"[{'bold red'}]Opção inválida.\n")
+            sleep(1)
+    else:
+        console.print(f"[{'bold red'}]Opção inválida.\n")
+        sleep(1)
---
+++ client/client_vigilance_hub.py
@@ -0,0 +1,441 @@
+import requests
+import json
+
+baseurl = 'http://127.0.0.1:9501/api/v1'
+
+
+def create_user(inputs):
+    url = baseurl + '/usuario/register'
+
+    data = {
+        "nome": inputs['nome'],
+        "email": inputs['email'],
+        "senha": inputs['senha'],
+        "isAdmin": inputs['isAdmin']
+    }
+
+    headers = {'content-type': 'application/json'}
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    if (response.status_code == 201):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def update_user(inputs):
+    url = baseurl + '/usuario/update/' + inputs['id_usuario']
+
+    data = {
+        "nome": inputs['nome'],
+        "email": inputs['email'],
+    }
+
+    headers = {'content-type': 'application/json'}
+    response = requests.put(url, headers=headers, json=data)
+
+    if (response.status_code == 201):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def delete_user_admin(inputs):
+    url = baseurl + '/usuario/admin/' + inputs['id_admin']
+
+    headers = {'content-type': 'application/json'}
+    response = requests.delete(url, headers=headers)
+
+    if (response.status_code == 200):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def delete_user_cliente(inputs):
+    url = baseurl + '/usuario/cliente/' + inputs['id_cliente']
+
+    headers = {'content-type': 'application/json'}
+    response = requests.delete(url, headers=headers)
+
+    if (response.status_code == 200):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def create_application(inputs):
+    url = baseurl + '/aplicacao'
+    headers = {'Content-Type': 'application/json'}
+
+    data = {
+        "idCliente": inputs['idCliente'],
+        "nome": inputs['nome']
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if (response.status_code == 201):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def update_application(inputs):
+    url = baseurl + '/aplicacao/' + inputs['id_aplicacao']
+    headers = {'Content-Type': 'application/json'}
+
+    data = {
+        "nome": inputs['nome']
+    }
+
+    response = requests.put(url, headers=headers, json=data)
+
+    if (response.status_code == 201):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def delete_application(inputs):
+    url = baseurl + '/aplicacao/' + inputs['id_aplicacao']
+
+    headers = {
+        'clienteToken': inputs['clienteToken'],
+        'content-type': 'application/json'
+    }
+    response = requests.delete(url, headers=headers)
+
+    if (response.status_code == 200):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def create_monitoramento_cache(inputs):
+    url = baseurl + '/monitoramento/cache'
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        "acao": inputs['acao'],
+        "chave": inputs['chave']
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    if (response.status_code == 201):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def update_monitoramento_cache(inputs):
+    url = baseurl + '/monitoramento/cache/' + inputs['id_cache']
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        "acao": inputs['acao'],
+        "chave": inputs['chave']
+    }
+
+    response = requests.put(url, headers=headers, json=data)
+    if (response.status_code == 200):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def create_monitoramento_depuracao(inputs):
+    url = baseurl + '/monitoramento/depuracao'
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        "depuracao": inputs['depuracao']
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if (response.status_code == 201):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def update_monitoramento_depuracao(inputs):
+    url = baseurl + '/monitoramento/depuracao/' + inputs['id_depuracao']
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        "depuracao": inputs['depuracao']
+    }
+
+    response = requests.put(url, headers=headers, json=data)
+    if (response.status_code == 200):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def create_monitoramento_erro(inputs):
+    url = baseurl + '/monitoramento/erro'
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        "nivel": inputs['nivel'],
+        "localizacao": inputs['localizacao'],
+        "stacktrace": inputs['stacktrace']
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if (response.status_code == 201):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def update_monitoramento_erro(inputs):
+    url = baseurl + '/monitoramento/erro/' + inputs['id_erro']
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        "nivel": inputs['nivel'],
+        "localizacao": inputs['localizacao'],
+        "stacktrace": inputs['stacktrace']
+    }
+
+    response = requests.put(url, headers=headers, json=data)
+
+    if (response.status_code == 200):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def create_monitoramento_log(inputs):
+    url = baseurl + '/monitoramento/log'
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        "nivel": inputs['nivel'],
+        "mensagem": inputs['mensagem']
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if (response.status_code == 201):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def update_monitoramento_log(inputs):
+    url = baseurl + '/monitoramento/log/' + inputs['id_log']
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        "nivel": inputs['nivel'],
+        "mensagem": inputs['mensagem']
+    }
+
+    response = requests.put(url, headers=headers, json=data)
+
+    if (response.status_code == 200):
+        return response.json()['message']
+    else:
+        return response.json()['erros']
+
+
+def create_monitoramento_query(inputs):
+    url = baseurl + '/monitoramento/query'
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'Content-Type': 'application/json'
+    }
+    data = {
+        "duracao": inputs['duracao'],
+        "conector": inputs['conector'],
+        "localizacao": inputs['localizacao'],
+        "query": inputs['query']
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    if (response.status_code == 201):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def update_monitoramento_query(inputs):
+    url = baseurl + '/monitoramento/query/' + inputs['id_query']
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'Content-Type': 'application/json'
+    }
+    data = {
+        "duracao": inputs['duracao'],
+        "conector": inputs['conector'],
+        "localizacao": inputs['localizacao'],
+        "query": inputs['query']
+    }
+
+    response = requests.put(url, headers=headers, json=data)
+
+    if (response.status_code == 200):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def create_monitoramento_request(inputs):
+    url = baseurl + '/monitoramento/request'
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        "duracao": inputs['duracao'],
+        "status": inputs['status'],
+        "metodo": inputs['metodo'],
+        "uri": inputs['uri'],
+        "headers": inputs['headers'],
+        "response": inputs['response']
+    }
+
+    response = requests.post(url, headers=headers, json=json.dumps(data))
+
+    if (response.status_code == 201):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def update_monitoramento_request(inputs):
+    url = baseurl + '/monitoramento/request/' + inputs['id_request']
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        "duracao": inputs['duracao'],
+        "status": inputs['status'],
+        "metodo": inputs['metodo'],
+        "uri": inputs['uri'],
+        "headers": inputs['headers'],
+        "response": inputs['response']
+    }
+
+    response = requests.put(url, headers=headers, json=data)
+
+    if (response.status_code == 200):
+        return response.json()
+    else:
+        return response.json()['erros']
+    
+
+def delete_monitoramento(inputs):
+    url = baseurl + '/monitoramento/' + inputs['id_monitoramento']
+
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'content-type': 'application/json'
+    }
+    response = requests.delete(url, headers=headers)
+
+    if (response.status_code == 200):
+        return response.json()
+    else:
+        return response.json()['erros']
+
+
+def total_aplicacoes(inputs):
+    url = baseurl + '/monitoramento/getTotalAplicacoes'
+
+    headers = {
+        'content-type': 'application/json'
+    }
+    response = requests.get(url, headers=headers)
+
+    if (response.status_code == 200):
+        return response.json()['response']
+    else:
+        return response.json()['erros']
+    
+    
+def total_monitoramentos(inputs):
+    url = baseurl + '/monitoramento/getTotalMonitoramentos'
+
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'content-type': 'application/json'
+    }
+    response = requests.get(url, headers=headers)
+
+    if (response.status_code == 200):
+        return response.json()['response']
+    else:
+        return response.json()['erros']
+
+
+def total_monitoramento_data(inputs):
+    url = baseurl + '/monitoramento/getTotalMonitoramentoData'
+
+    headers = {
+        'aplicacaoToken': inputs['aplicacaoToken'],
+        'clienteToken': inputs['clienteToken'],
+        'content-type': 'application/json'
+    }
+    response = requests.get(url, headers=headers)
+
+    if (response.status_code == 200):
+        return response.json()['response']
+    else:
+        return response.json()['erros']
+
+# create_monitoramento_query({
+#     'duracao': 3.15,
+#     'conector': 'MYSQL',
+#     'localizacao': 'CLASS/APICONTROLLER',
+#     'query': 'select * from vigilance_hub.cliente c',
+#     'aplicacaoToken': '51107922ed43d5acd7622aad8c2301cd75dab9005f4e6571986c0e96fc08879b60ce6017afa09cc8c82d1b0706f50bb9b9ad982bb78c1949e97dc1de468d84db',
+#     'clienteToken': '0ecfaf4fdbc5cd4568a8eabf46e3e47ed7c9f749dce6bc5e4ea4cd445451c4274192ed392f35d68ff6c2c77a803dc3df66b8281fd44ba54f0c6a9914c4d2133d'
+# })
